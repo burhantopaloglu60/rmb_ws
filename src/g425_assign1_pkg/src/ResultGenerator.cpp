331cb3b3a0072272de60f4a7d086044e1d481612
rik0905 <51375105+rik0905@users.noreply.github.com>
2025-09-30 16:13:54 +0200

    Update ResultGenerator.cpp

--- src/g425_assign1_pkg/src/ResultGenerator.cpp
+++ src/g425_assign1_pkg/src/ResultGenerator.cpp
@@ -1,7 +1,7 @@
 /*
 Node description: what is the node doing , what are the node objects used 
 Tentamen Result Generator Node
-WallTimer + Publisher + Subscriber
+WallTimer + ResultGenerator + Subscriber
 
 "The tentamen result generator node collects from a Database (file) all student/course
 combinations for which ”tentamen” results need to be generated. These are the
@@ -14,4 +14,64 @@ process"
 
 Rik
 */ 
+
+/*
+--Software changes:
+one line per change 
+(1) created 30.9.2025: developer-Rik van Velzen reviewer(s)-X
+(2) changed 01.4.2025: xxx functionality added ... : developer-Rik van Velzen reviewer(s)-X
+...
+*/
+
+//-- tester: X
+
+#include "rclcpp/rclcpp.hpp"
+//--custom includes  
+#include "rmb25_assign1_interfaces_pkg" 
+
+
+class ResultGenerator : public rclcpp::Node
+{
+	public:
+	//-- constuctor: 
+	ResultGenerator() : Node("ResultGenerator_node")
+	{	
+		//--communication and timer objects: 
+		ResultGenerator_theanswer_ = this->create_ResultGenerator<std_msgs::msg::Int32>("the_answer",1);
+		timer_theanswer_ = this->create_wall_timer(
+ 	    std::chrono::milliseconds(1000), std::bind(&ResultGenerator::timer_theanswer_function, this));
+		
+		//--customs functions:
+		
+
+	}
+
+	//-- communication and timer functions 
+	void timer_theanswer_function()
+	{   
+        number_.data = rand() % 91 + 10;
+		ResultGenerator_theanswer_ ->publish(number_) ;
+		/*your code where you publish*/
+	}
+
+
+	private:
+		//--rclcpp variables:
+
+		rclcpp::ResultGenerator<std_msgs::msg::Int32>::SharedPtr ResultGenerator_theanswer_;
+		rclcpp::TimerBase::SharedPtr timer_theanswer_ ;
+		
+		//--custom variables:
+		std_msgs::msg::Int32 number_;
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<ResultGenerator>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
+
 

5c80bb77bbd54bc414c625976e8f753722348ba2
MelProjects <168737411+MelProjects@users.noreply.github.com>
2025-10-01 20:24:19 +0200

    Update RetakeScheduler, RetakeGradeDeterminator and Retaker.action

--- src/g425_assign1_pkg/src/RetakeGradeDeterminator.cpp
+++ src/g425_assign1_pkg/src/RetakeGradeDeterminator.cpp
@@ -14,3 +14,92 @@ not be overwritten).
 Melissa
 */ 
 
+/*
+--Software changes:
+one line per change 
+(1) created 01.10.2025: developer-Melissa van Leeuwen reviewer(s)-X
+...
+*/
+
+//-- tester: X
+
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include "g425_assign1_interfaces_pkg/action/retaker.hpp"
+
+using Retaker = g425_assign1_interfaces_pkg::action::Retaker;
+using GoalHandleRetaker = rclcpp_action::ServerGoalHandle<Retaker>;
+
+class RetakeGradeDeterminator : public rclcpp::Node
+{
+public:
+    RetakeGradeDeterminator()  : Node("retake_grade_determinator")
+    {
+        retake_actionserver_ = rclcpp_action::create_server<Retaker>(
+            this,
+            "retaker",
+            std::bind(&RetakeGradeDeterminator::handle_goal, this, std::placeholders::_1, std::placeholders::_2),
+            std::bind(&RetakeGradeDeterminator::handle_cancel, this, std::placeholders::_1),
+            std::bind(&RetakeGradeDeterminator::handle_accepted, this, std::placeholders::_1));
+    }
+
+private:
+    rclcpp_action::Server<Retaker>::SharedPtr retake_actionserver_;
+
+    rclcpp_action::GoalResponse handle_goal(
+        const rclcpp_action::GoalUUID &,
+        std::shared_ptr<const Retaker::Goal> goal)
+    {
+        RCLCPP_INFO(this->get_logger(), "Received retake for student: %s", goal->student.c_str());
+        return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+    }
+
+    rclcpp_action::CancelResponse handle_cancel(const std::shared_ptr<GoalHandleRetaker>)
+    {
+        RCLCPP_INFO(this->get_logger(), "Retake cancelled");
+        return rclcpp_action::CancelResponse::ACCEPT;
+    }
+
+    void handle_accepted(const std::shared_ptr<GoalHandleRetaker> goal_handle)
+    {
+        std::thread{std::bind(&RetakeGradeDeterminator::execute, this, goal_handle)}.detach();
+    }
+
+    void execute(const std::shared_ptr<GoalHandleRetaker> goal_handle)
+    {
+        auto feedback = std::make_shared<Retaker::Feedback>();
+        auto result = std::make_shared<Retaker::Result>();
+
+        // Simulate exam results instead of using result generator and grade calculator
+        int sum = 0;
+        int n = 3;
+        for (int i = 0; i < n; i++) {
+            int grade = 10 + (rand() % 91); // random grade between 10 to 100
+            sum += grade;
+
+            RCLCPP_INFO(this->get_logger(), "Tentamen result %d received: %d", i + 1, grade);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+
+        int final_grade = sum / n;
+        if (final_grade < 10) final_grade = 10;
+        if (final_grade > 100) final_grade = 100;
+
+        if (final_grade >= 55) result->success = true;
+        if (final_grade < 55) result->success = false;
+        result->message = "Final grade calculated";
+
+        goal_handle->succeed(result);
+
+        RCLCPP_INFO(this->get_logger(), "Retake completed with final grade: %d", final_grade);
+    }
+
+};
+
+int main(int argc, char ** argv)
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<RetakeGradeDeterminator>());
+    rclcpp::shutdown();
+    return 0;
+}
--- src/g425_assign1_pkg/src/RetakeScheduler.cpp
+++ src/g425_assign1_pkg/src/RetakeScheduler.cpp
@@ -9,3 +9,68 @@ requests to start a new cijfer determination."
 Melissa
 */ 
 
+/*
+--Software changes:
+one line per change 
+(1) created 01.10.2025: developer-Melissa van Leeuwen reviewer(s)-X
+...
+*/
+
+//-- tester: X
+
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include "g425_assign1_interfaces_pkg/action/retaker.hpp"
+
+using Retaker = g425_assign1_interfaces_pkg::action::Retaker;
+using RetakeGoalHandle = rclcpp_action::ClientGoalHandle<Retaker>;
+
+class RetakeScheduler : public rclcpp::Node
+{
+public:
+    RetakeScheduler() : Node("retake_scheduler")
+    {
+        retake_actionclient_ = rclcpp_action::create_client<Retaker>(this, "retaker");
+        timer_ = this->create_wall_timer(std::chrono::seconds(5), std::bind(&RetakeScheduler::check_and_schedule, this));
+    }
+
+private:
+    rclcpp_action::Client<Retaker>::SharedPtr retake_actionclient_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    void check_and_schedule()
+    {
+        retake_actionclient_->wait_for_action_server();
+
+        // TO DO collecting students who failed from database 
+
+        // Example student
+        auto goal_msg = Retaker::Goal();
+        goal_msg.student = "Melissa";   // teststudent
+
+        RCLCPP_INFO(this->get_logger(), "Start retake for %s", goal_msg.student.c_str());
+
+        auto send_goal_options = rclcpp_action::Client<Retaker>::SendGoalOptions();
+        send_goal_options.result_callback =
+        [](const RetakeGoalHandle::WrappedResult & result) {
+            if (result.code == rclcpp_action::ResultCode::SUCCEEDED) {
+                RCLCPP_INFO(rclcpp::get_logger("RetakeScheduler"),
+                            "Retake completed: %s, message: %s",
+                            result.result->success ? "true" : "false",
+                            result.result->message.c_str());
+            } else {
+                RCLCPP_ERROR(rclcpp::get_logger("RetakeScheduler"), "Retake scheduling failed");
+            }
+        };
+
+        retake_actionclient_->async_send_goal(goal_msg, send_goal_options);
+    }
+};
+
+int main(int argc, char ** argv)
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<RetakeScheduler>());
+    rclcpp::shutdown();
+    return 0;
+}

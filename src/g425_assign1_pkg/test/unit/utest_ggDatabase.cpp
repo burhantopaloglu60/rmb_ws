e5f1be7050df3d4c8f569a1fa415595e34cf8d4e
Burhan Topaloglu <burhantopaloglu60@gmail.com>
2025-10-12 21:40:21 +0200

    Changed around placing of tests in ggdatabase tests

--- src/g425_assign1_pkg/test/unit/utest_ggDatabase.cpp
+++ src/g425_assign1_pkg/test/unit/utest_ggDatabase.cpp
@@ -12,8 +12,8 @@ class TestDB : public ::testing::Test
 protected:
   void SetUp() override
   {
-    db_ = new GGDatabase("localhost","john_gradegenerator","1234","grade_generator");
-    //specifically defining the constructor values here so you can change it to a test database while testing
+    db_ = new GGDatabase("localhost", "john_gradegenerator", "1234", "grade_generator");
+    // specifically defining the constructor values here so you can change it to a test database while testing
   }
   void TearDown() override
   {
@@ -33,9 +33,15 @@ TEST_F(TestDB, TestSimpleFunctions)
 
   EXPECT_EQ(db_->getGradeAmountFromCourse(db_->getCourseId("Mathematics")), 5);
 
+  std::vector<std::tuple<int, int>> student_course_relations = db_->getAllStudentCoursesRel();
+  EXPECT_GT(student_course_relations.size(), 0u);
+}
+
+TEST_F(TestDB, LongerTests)
+{
   bool added_and_found_final_grade = false;
-  DBT_Grade grdToAdd{0, 1, 1, 60.0};;
-  DBT_FinalGrade finalGrdToAdd{0, 1, 1, 5, 60.0};;
+  DBT_Grade grdToAdd{ 0, 1, 1, 60.0 };
+  DBT_FinalGrade finalGrdToAdd{ 0, 1, 1, 5, 60.0 };
 
   EXPECT_TRUE(db_->addGrade(grdToAdd));
   EXPECT_TRUE(db_->addFinalGrade(finalGrdToAdd));
@@ -44,23 +50,17 @@ TEST_F(TestDB, TestSimpleFunctions)
 
   for (const auto& fg : fnl_grades)
   {
-    if (fg.student_id == finalGrdToAdd.student_id && fg.course_id == finalGrdToAdd.course_id && //
-      fg.number_of_exams == finalGrdToAdd.number_of_exams && fg.final_grade == finalGrdToAdd.final_grade)
+    if (fg.student_id == finalGrdToAdd.student_id && fg.course_id == finalGrdToAdd.course_id &&  //
+        fg.number_of_exams == finalGrdToAdd.number_of_exams && fg.final_grade == finalGrdToAdd.final_grade)
     {
       added_and_found_final_grade = true;
     }
   }
   EXPECT_TRUE(added_and_found_final_grade);
-}
 
-TEST_F(TestDB, LongerTests)
-{
-  std::vector<std::tuple<int, int>> student_course_relations = db_->getAllStudentCoursesRel();
+  // will be longer. needs to take into account situation where there are no more missing final grades
   std::vector<std::tuple<int, int>> missing_final_grades = db_->getMissingFinalGrades();
-
-
-  EXPECT_GT(student_course_relations.size(), 0u);
-  EXPECT_GT(missing_final_grades.size(), 0u); //this one can be difficult to test if there are no more missing final grades
+  EXPECT_GT(missing_final_grades.size(), 0u);
 }
 
 int main(int argc, char** argv)

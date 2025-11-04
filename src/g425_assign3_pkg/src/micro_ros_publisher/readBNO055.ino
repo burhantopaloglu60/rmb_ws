2143fc92c699c3a53b6890495bd3d0dfa27d54d9
rik0905 <51375105+rik0905@users.noreply.github.com>
2025-11-04 16:31:41 +0100

    msg and example code removed

--- src/g425_assign3_pkg/src/micro_ros_publisher/readBNO055.ino
+++ src/g425_assign3_pkg/src/micro_ros_publisher/readBNO055.ino
@@ -1,8 +1,9 @@
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>
+// #include <Wire.h>
+// #include <Adafruit_Sensor.h>
+// #include <Adafruit_BNO055.h>
+// #include <utility/imumaths.h>
 
+<<<<<<< Updated upstream
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 long prevTimestamp = 0;
 double prevAngVelocity = 0;
@@ -57,3 +58,42 @@ void loop() {
   delay(200); // polling timer
 }
 
+=======
+// Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+// void setup() 
+// {
+//   Serial.begin(9600);
+
+//   bno.begin();
+//   bno.setExtCrystalUse(true);
+// }
+// //timestamp, x, y, z, rotation acceleration. or at least what I understood of it
+// void loop() {
+//   sensors_event_t accelEvent;
+//   sensors_event_t gyroEvent;
+
+//   // Get linear acceleration and gyroscope data
+//   bno.getEvent(&accelEvent, Adafruit_BNO055::VECTOR_ACCELEROMETER);
+//   bno.getEvent(&gyroEvent, Adafruit_BNO055::VECTOR_GYROSCOPE);
+
+//   // Print timestamp
+//   Serial.print("Timestamp: ");
+//   Serial.print(accelEvent.timestamp);
+//   Serial.print(" ms");
+
+//   // Print x, y, z acceleration
+//   Serial.print(" | Accel (m/s²): ");
+//   Serial.print(accelEvent.acceleration.x); Serial.print(", ");
+//   Serial.print(accelEvent.acceleration.y); Serial.print(", ");
+//   Serial.print(accelEvent.acceleration.z);
+
+//   // Print rotation acceleration (angular velocity?)
+//   Serial.print(" | Rotation Accel (rad/s): ");
+//   Serial.print(gyroEvent.gyro.x); Serial.print(", ");
+//   Serial.print(gyroEvent.gyro.y); Serial.print(", ");
+//   Serial.println(gyroEvent.gyro.z);
+
+//   delay(100);
+// }
+>>>>>>> Stashed changes

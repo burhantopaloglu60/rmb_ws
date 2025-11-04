feb96a4b90662aed56128c09b4bc373e73bab98d
Burhan Topaloglu <burhantopaloglu60@gmail.com>
2025-11-04 16:00:23 +0100

    Update readBNO055.ino

--- src/g425_assign3_pkg/src/micro_ros_publisher/readBNO055.ino
+++ src/g425_assign3_pkg/src/micro_ros_publisher/readBNO055.ino
@@ -4,39 +4,56 @@
 #include <utility/imumaths.h>
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
+long prevTimestamp = 0;
+double prevAngVelocity = 0;
 
-void setup() 
-{
+long timestamp = 0; 
+double angVelocity = 0;
+double orientationVect[3] = {0,0,0}; //x,y,z,
+
+double angularAcceleration = 0.0f;
+
+void setup() {
   Serial.begin(9600);
 
   bno.begin();
   bno.setExtCrystalUse(true);
 }
-//timestamp, x, y, z, rotation acceleration. or at least what I understood of it
+// output to ros = (timestamp IRL + x orientation + y orientation + z orientation + angular acceleration from topdown)
+// Angular accel (rad/s²), xyz (Quaternion)
 void loop() {
-  sensors_event_t accelEvent;
+  //sensors_event_t accelEvent; the accelerometer has no angular acceleration, not needed
   sensors_event_t gyroEvent;
 
   // Get linear acceleration and gyroscope data
-  bno.getEvent(&accelEvent, Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  //bno.getEvent(&accelEvent, Adafruit_BNO055::VECTOR_ACCELEROMETER);
   bno.getEvent(&gyroEvent, Adafruit_BNO055::VECTOR_GYROSCOPE);
 
-  // Print timestamp
-  Serial.print("Timestamp: ");
-  Serial.print(accelEvent.timestamp);
-  Serial.print(" ms");
+  //throw everything in to variables!
+  angVelocity = gyroEvent.gyro.z;
+  orientationVect[0] = gyroEvent.orientation.x;
+  orientationVect[1] = gyroEvent.orientation.y;
+  orientationVect[2] = gyroEvent.orientation.z;
+  timestamp = gyroEvent.timestamp;
 
-  // Print x, y, z acceleration
-  Serial.print(" | Accel (m/s²): ");
-  Serial.print(accelEvent.acceleration.x); Serial.print(", ");
-  Serial.print(accelEvent.acceleration.y); Serial.print(", ");
-  Serial.print(accelEvent.acceleration.z);
+  if(prevTimestamp > 0 )
+  {
+    // calculate angular acceleration and send it in ros message
+    // angular acceleration = dω​ / dt 
+    angularAcceleration = ( (angVelocity - prevAngVelocity) / ( (double)(timestamp - prevTimestamp)/1000) ); // we get dt in seconds and divide the delta velocity by it 
+    Serial.print("orientation x: ");
+    Serial.print(orientationVect[0]);
+    Serial.print(" | orientation y: ");
+    Serial.print(orientationVect[1]);
+    Serial.print(" | orientation z: ");
+    Serial.print(orientationVect[2]);
+    Serial.print(" | rotation accel in rad/s²: ");
+    Serial.println(angularAcceleration);
 
-  // Print rotation acceleration (angular velocity?)
-  Serial.print(" | Rotation Accel (rad/s): ");
-  Serial.print(gyroEvent.gyro.x); Serial.print(", ");
-  Serial.print(gyroEvent.gyro.y); Serial.print(", ");
-  Serial.println(gyroEvent.gyro.z);
+  }
+  prevAngVelocity = angVelocity;
+  prevTimestamp = timestamp; // save previous timestamp to get angular accel
 
-  delay(100);
+  delay(200); // polling timer
 }
+

3b769b285c7099fb62f6fe0fff5fe5e6cbeabdeb
rik0905 <51375105+rik0905@users.noreply.github.com>
2025-11-25 16:30:00 +0100

    Update position_approximator_mecanum.cpp

--- src/g425_assign4_pkg/src/position_approximator_mecanum.cpp
+++ src/g425_assign4_pkg/src/position_approximator_mecanum.cpp
@@ -13,6 +13,7 @@ Software changes (one line by change):
 
 using namespace std::placeholders;
 using mecanum = g425_assign4_interfaces_pkg::msg::Mecanum;
+using PositionData = g425_assign4_interfaces_pkg::msg::PositionData;
 
 class position_approximator_mecanum : public rclcpp::Node
 {
@@ -22,21 +23,26 @@ public:
         // mecanum_sub_ = this->create_subscription<mecanum>(
         // "mecanum_sim", 10,
         // std::bind(&position_approximator_mecanum::calculate_mecanum, this, _1));
-        mecanum_pub_ = this->create_publisher<mecanum>("mecanum_data", 10);
+        mecanum_pub_ = this->create_publisher<PositionData>("mecanum_pos", 10);
 
         RCLCPP_INFO(this->get_logger(), "position_approximator_mecanum_node active.");
     }
 private:
     void calculate_mecanum(const mecanum &msg)
     {
-        double wfl = msg.wfl;
-        double wfr = msg.wfr;
-        double wrl = msg.wrl;
-        double wrr = msg.wrr;
+        // double wfl = msg.wfl;
+        // double wfr = msg.wfr;
+        // double wrl = msg.wrl;
+        // double wrr = msg.wrr;
+        double wfl = 1;
+        double wfr = 1;
+        double wrl = 1;
+        double wrr = 1;
         double r = 0.05; // meters
         double l = 0.3; // meters
         double w = 0.2; // meters
-        double t = 0.1; // time interval in seconds
+        // t = msg->stamp - last_stamp_; // time interval in seconds
+        t = 0;
         double vx = (r / 4) * (wfl + wfr + wrl + wrr);
         double vy = (r / 4) * (-wfl + wfr + wrl - wrr);
         double omega = (r / (4 * (l + w))) * (-wfl + wfr - wrl + wrr);
@@ -49,14 +55,22 @@ private:
         RCLCPP_INFO(this->get_logger(),
                     "Updated Position: x=%.2f m, y=%.2f m, theta=%.2f rad",
                     px_, py_, theta_);
+        PositionData data;
+        data.x = px_;
+        data.y = py_;
+        data.z = 0.0;
+        data.yaw_z = theta_;
+        mecanum_pub_->publish(data);
+        // last_stamp_ = msg.header.stamp;
     }
     double px_ = 0.0;
     double py_ = 0.0;
     double theta_ = 0.0;
-    rclcpp::Time last_stamp_;
+    double t = 0;
+    // rclcpp::Time last_stamp_ = rclcpp::Time(now());
     bool first_msg_ = true;
     rclcpp::Subscription<mecanum>::SharedPtr mecanum_sub_;
-    rclcpp::Publisher<mecanum>::SharedPtr mecanum_pub_;
+    rclcpp::Publisher<PositionData>::SharedPtr mecanum_pub_;
 };
 int main(int argc, char **argv)
 {
